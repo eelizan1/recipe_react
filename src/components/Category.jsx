6cf6f67e76d3cb488e4ca870b7586d8e2778c37c
Enrico Elizan Jr <eelizanjr@outlook.com>
2022-12-08 17:41:15 -0500

    removed nav link

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,7 +1,6 @@
 import { FaPizzaSlice, FaHamburger } from "react-icons/fa";
 import { GiNoodles, GiChopsticks } from "react-icons/gi";
 import styled from "styled-components";
-import { NavLink } from "react-router-dom";
 
 import React from "react";
 
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import Home from './Home'
+import React from "react";
+import Home from "./Home";
 
 function Pages() {
   return (
-    <div><Home/></div>
-  )
+    <div>
+      <Home />
+    </div>
+  );
 }
 
-export default Pages
+export default Pages;
