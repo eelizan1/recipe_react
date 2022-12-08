53b2b1dcb11f7ba4e2c1959af2aa573847f36332
Enrico Elizan Jr <eelizanjr@outlook.com>
2022-12-08 17:39:18 -0500

    Added veggie page

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -17,12 +17,21 @@ function Popular() {
 
   // 1. Define how we fetch the data
   const getPopular = async () => {
-    // async so that we can wait for data before rendering out the page
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
-    );
-    const data = await api.json(); // process api result into json format
-    setPopular(data.recipes);
+    const check = localStorage.getItem("popular");
+
+    if (check) {
+      // get from local storage if available
+      setPopular(JSON.parse(check));
+    } else {
+      // async so that we can wait for data before rendering out the page
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+      );
+      const data = await api.json();
+
+      localStorage.setItem("popular", JSON.stringify(data.recipes));
+      setPopular(data.recipes);
+    }
   };
 
   return (
@@ -42,7 +51,7 @@ function Popular() {
         >
           {popular.map((recipe) => {
             return (
-              <SplideSlide>
+              <SplideSlide key={recipe.id}>
                 <Card>
                   <p>{recipe.title}</p>
                   <img src={recipe.image} alt={recipe.title} />
@@ -62,43 +71,43 @@ const Wrapper = styled.div`
 `;
 
 const Card = styled.div`
-  min-height: 25rem;
+  min-height: 20rem;
   border-radius: 2rem;
   overflow: hidden;
-  position: relative; 
+  position: relative;
 
   img {
     border-radius: 2rem;
-    position: absolute; 
-    left: 0; 
-    width: 100%; 
-    height: 100%; 
-    object-fit: cover; 
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
 
   p {
-    position: absolute; 
-    z-index: 10; 
-    left: 50%; 
-    bottom: 0%; 
-    transform: translate(-50%, 0%); 
-    color: white; 
-    width: 100%; 
-    text-align: center; 
-    font-weight: 600; 
-    font-szie: 1rem; 
-    height: 40%; 
-    display: flex; 
-    justify-content: center; 
-    align-items: center; 
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `;
 
 const Gradient = styled.div`
-  z-index: 3; 
-  position: absolute; 
-  width: 100%; 
-  height: 100%; 
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
   background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
 `;
 
