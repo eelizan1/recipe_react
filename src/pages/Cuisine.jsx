468dbd0e4c0bae4c018742589cead3dab7699e7e
Enrico Elizan Jr <eelizanjr@outlook.com>
2022-12-08 18:28:49 -0500

    Cuisine

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { Link, useParams } from "react-router-dom";
 
 function Cuisine() {
-  return <div>Cuisine</div>;
+  const [cuisine, setCuisine] = useState([]);
+  let params = useParams();
+
+  useEffect(() => {
+    getCuisine(params.type);
+  }, [params.type]);
+
+  const getCuisine = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
+    );
+    const recipes = await data.json();
+    setCuisine(recipes.results);
+  };
+  return <div></div>;
 }
 
 export default Cuisine;
