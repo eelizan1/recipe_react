0b7a854c0b3276e8d0b9141587ac0ff1e2394ffb
Enrico Elizan Jr <eelizanjr@outlook.com>
2022-12-09 15:02:49 -0500

    Cuisine page

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
 // import { motion } from "framer-motion";
 import { useParams } from "react-router-dom";
 
@@ -18,7 +18,40 @@ function Cuisine() {
     const recipes = await data.json();
     setCuisine(recipes.results);
   };
-  return <div>{cuisine}</div>;
+  return (
+    <Grid>
+      {cuisine.map((item) => {
+        return (
+          <Card key={item.id}>
+            <img src={item.image} alt="" />
+            <h4>{item.title}</h4>
+          </Card>
+        );
+      })}
+    </Grid>
+  );
 }
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+  img {
+    width: 100%;
+    border-radius: 2rem;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  h4 {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
 
 export default Cuisine;
